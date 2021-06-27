455390de6503cf0e5264e664b0440932d4be3e18
Nithin Mahendran <nithinmahendren@gmail.com>
2021-06-27 16:31:53 +0530

    Splash Screen added

---
+++ lib/UI/contact_screen.dart
@@ -0,0 +1,19 @@
+import 'package:contactsapp/global.dart';
+import 'package:flutter/material.dart';
+
+class ContactsScreen extends StatefulWidget {
+  const ContactsScreen({ Key? key }) : super(key: key);
+
+  @override
+  _ContactsScreenState createState() => _ContactsScreenState();
+}
+
+class _ContactsScreenState extends State<ContactsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      
+    );
+  }
+}
---
+++ lib/global.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+TextStyle splashScreen = TextStyle(
+  fontSize: 39.0,
+  color: Colors.black,
+  fontWeight: FontWeight.w600
+
+);
