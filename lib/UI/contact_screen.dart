3b948ad1acca05d9e074fc6b833e8bb91b4c37ec
Nithin Mahendran <nithinmahendren@gmail.com>
2021-06-29 20:20:27 +0530

    HomePage Initialized

--- lib/UI/contact_screen.dart
+++ lib/UI/contact_screen.dart
@@ -2,7 +2,7 @@ import 'package:contactsapp/global.dart';
 import 'package:flutter/material.dart';
 
 class ContactsScreen extends StatefulWidget {
-  const ContactsScreen({ Key? key }) : super(key: key);
+  const ContactsScreen({Key? key}) : super(key: key);
 
   @override
   _ContactsScreenState createState() => _ContactsScreenState();
@@ -13,7 +13,56 @@ class _ContactsScreenState extends State<ContactsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      
+      body: Align(
+        alignment: Alignment.center,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 80.0, left: 20.0, right: 20.0),
+          child: Column(
+            children: [
+              Text(
+                "Contacts",
+                style: introScreen,
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 25.0),
+                height: 40.0,
+                decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: const Color(0xffededed)),
+              ),
+              Container(
+                  margin: EdgeInsets.only(top: 25.0, right: 260.0),
+                  child: Text(
+                    "Branch",
+                    style: branchSelection,
+                    textAlign: TextAlign.start,
+                  )),
+              Container(
+                margin: EdgeInsets.only(top: 20.0),
+                height: 75.0,
+                width: 350,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      offset: const Offset(
+                        0.0,
+                        4.0,
+                      ),
+                      blurRadius: 8.0,
+                      spreadRadius: 0.0,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
             ),
           ),
         ),
-        home: SplashScreen());
+        home: ContactsScreen());
   }
 }
 
